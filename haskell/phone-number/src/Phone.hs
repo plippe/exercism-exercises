94e21580a9cc682fca13523d81d64906b99d7ba7
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-02 14:08:04 +0100

    Persistance is key

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -2,11 +2,8 @@ module Phone (number) where
 
 import Data.Char (isDigit)
 
-isCode :: Char -> Bool
-isCode n = elem n ['2'..'9']
-
 number :: String -> Maybe String
 number xs = case filter isDigit xs of
-    ns@['1',n,_,_,n',_,_,_,_,_,_] | isCode n && isCode n' -> Just $ tail ns
-    ns@[    n,_,_,n',_,_,_,_,_,_] | isCode n && isCode n' -> Just ns
+    ns@['1',n,_,_,n',_,_,_,_,_,_] | all (> '1') [n,n'] -> Just $ tail ns
+    ns@[    n,_,_,n',_,_,_,_,_,_] | all (> '1') [n,n'] -> Just ns
     _ -> Nothing
