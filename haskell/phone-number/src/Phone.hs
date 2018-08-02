4cd89d58bb1c554a11e7dd26520a9112666ba3cb
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-02 12:25:11 +0100

    Improve haskell phone

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,33 +1,14 @@
 module Phone (number) where
 
-import Data.Char (isDigit, intToDigit)
+import Data.Char (isDigit)
 
-isCode :: String -> Bool
-isCode ns = case length ns of
-    3 | elem (head ns) ['2'..'9'] -> True
-    _ -> False
+isCode :: Char -> Bool
+isCode n = elem n ['2'..'9']
 
-areaCode :: String -> Maybe String
-areaCode ns = case length ns of
-    10 -> Just $ take 3 ns
-    _ -> Nothing
-
-exchangeCode :: String -> Maybe String
-exchangeCode ns = case length ns of
-    10 -> Just $ take 3 $ drop 3 ns
-    _ -> Nothing
-
-tenDigitNumber :: String -> Maybe String
-tenDigitNumber xs = case length ns of
-    11 | head ns == '1' -> Just $ tail ns
-    10 -> Just ns
-    _ -> Nothing
-    where ns = filter isDigit xs
+number' :: String -> Maybe String
+number' ns@('1':n:_:_:n':_:_:_:_:_:_:[]) | isCode n && isCode n' = Just $ tail ns
+number' ns@(n:_:_:n':_:_:_:_:_:_:[])     | isCode n && isCode n' = Just ns
+number' _ = Nothing
 
 number :: String -> Maybe String
-number xs = do
-    ns <- tenDigitNumber xs
-    areaCode' <- areaCode ns
-    exchangeCode' <- exchangeCode ns
-    if (isCode areaCode') && (isCode exchangeCode') then Just ns
-    else Nothing
+number = number' . filter isDigit
