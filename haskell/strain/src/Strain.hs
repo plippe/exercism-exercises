a621597cb3a784d7b63c145acf8879e5ca5071e6
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-02 08:51:14 +0100

    Improve haskell strain

--- haskell/strain/src/Strain.hs
+++ haskell/strain/src/Strain.hs
@@ -1,10 +1,10 @@
 module Strain (keep, discard) where
 
+import Data.Bool (bool)
+
 discard :: (a -> Bool) -> [a] -> [a]
 discard p = keep (not . p)
 
 keep :: (a -> Bool) -> [a] -> [a]
 keep _ [] = []
-keep p (x:xs) = case p x of
-    True -> x : keep p xs
-    False -> keep p xs
+keep p (x:xs) = bool [] [x] (p x) ++ keep p xs
