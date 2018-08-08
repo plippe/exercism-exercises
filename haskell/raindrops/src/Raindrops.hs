f9b4678ecf7d06456e98041d815f4743e8001812
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-08 09:51:53 +0100

    Embrace empty string as value

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -2,18 +2,15 @@ module Raindrops (convert) where
 
 import Data.Bool (bool)
 
-values :: [(Int, String)]
-values =
+intAndValue :: [(Int, String)]
+intAndValue =
     [ (3, "Pling")
     , (5, "Plang")
     , (7, "Plong")
     ]
 
-modValues :: [(Int, String)] -> Int -> [String]
-modValues [] n = []
-modValues ((i, str):xs) n = bool [] [str] (mod n i == 0) ++ modValues xs n
-
 convert :: Int -> String
-convert n = case modValues values n of
-    [] -> show n
-    xs -> concat xs
+convert n = ifEmptyShowN . concatMap ifModValue $ intAndValue
+    where
+        ifModValue (i, v) = bool "" v (mod n i == 0)
+        ifEmptyShowN xs = bool xs (show n) (null xs)
