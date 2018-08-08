53cb81acf5c072f00936555e8b1272e0e9783489
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-08 09:23:51 +0100

    Use replicate over take and cycle

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -2,15 +2,13 @@ module Diamond (diamond) where
 
 import Data.Char (isAlpha)
 
-padding :: Int -> Int -> [Char]
-padding l i = take (l - i) $ cycle " "
-
-margin :: Int -> [Char]
-margin i = take (i * 2 - 1) $ cycle " "
-
 format :: Int -> (Char, Int) -> [Char]
-format l ('A', i) = (padding l i) ++ "A" ++ (padding l i)
-format l (x, i) = (padding l i) ++ [x] ++ (margin i) ++ [x] ++ (padding l i)
+format l (x, i) = case x of
+    'A' -> padding ++ "A" ++ padding
+    x   -> padding ++ [x] ++ margin  ++ [x] ++ padding
+    where
+        padding = replicate (l - i) ' '
+        margin = replicate (i * 2 - 1) ' '
 
 diamond :: Char -> Maybe [String]
 diamond c
