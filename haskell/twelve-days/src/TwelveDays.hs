debabf16363725bd225b80741c5b20bfcb92ab71
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-08 09:56:09 +0100

    Remove useless function

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -4,17 +4,17 @@ import Data.Maybe (fromMaybe)
 
 gift :: Int -> Maybe String
 gift 1 = Just "a Partridge in a Pear Tree."
-gift 2 = fmap (\g -> "two Turtle Doves, and " ++ g) $ gift 1
-gift 3 = fmap (\g ->"three French Hens, " ++ g) $ gift 2
-gift 4 = fmap (\g -> "four Calling Birds, " ++ g) $ gift 3
-gift 5 = fmap (\g -> "five Gold Rings, " ++ g) $ gift 4
-gift 6 = fmap (\g -> "six Geese-a-Laying, " ++ g) $ gift 5
-gift 7 = fmap (\g -> "seven Swans-a-Swimming, " ++ g) $ gift 6
-gift 8 = fmap (\g -> "eight Maids-a-Milking, " ++ g) $ gift 7
-gift 9 = fmap (\g -> "nine Ladies Dancing, " ++ g) $ gift 8
-gift 10 = fmap (\g -> "ten Lords-a-Leaping, " ++ g) $ gift 9
-gift 11 = fmap (\g -> "eleven Pipers Piping, " ++ g) $ gift 10
-gift 12 = fmap (\g -> "twelve Drummers Drumming, " ++ g) $ gift 11
+gift 2 = fmap ("two Turtle Doves, and " ++) $ gift 1
+gift 3 = fmap ("three French Hens, " ++) $ gift 2
+gift 4 = fmap ("four Calling Birds, " ++) $ gift 3
+gift 5 = fmap ("five Gold Rings, " ++) $ gift 4
+gift 6 = fmap ("six Geese-a-Laying, " ++) $ gift 5
+gift 7 = fmap ("seven Swans-a-Swimming, " ++) $ gift 6
+gift 8 = fmap ("eight Maids-a-Milking, " ++) $ gift 7
+gift 9 = fmap ("nine Ladies Dancing, " ++) $ gift 8
+gift 10 = fmap ("ten Lords-a-Leaping, " ++) $ gift 9
+gift 11 = fmap ("eleven Pipers Piping, " ++) $ gift 10
+gift 12 = fmap ("twelve Drummers Drumming, " ++) $ gift 11
 gift _ = Nothing
 
 nth :: Int -> Maybe String
