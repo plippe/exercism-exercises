e47759480532a27e3b685f45d8d10f9014e14caa
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-16 22:20:27 +0100

    Point free square method

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -3,11 +3,8 @@ module Squares (difference, squareOfSums, sumOfSquares) where
 difference :: Integral a => a -> a
 difference n = squareOfSums n - sumOfSquares n
 
-square :: Integral a => a -> a
-square n = n ^ 2
-
 squareOfSums :: Integral a => a -> a
-squareOfSums n = square $ sum [1..n]
+squareOfSums n = (^ 2) $ sum [1..n]
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = sum $ map square [1..n]
+sumOfSquares n = sum $ map (^ 2) [1..n]
