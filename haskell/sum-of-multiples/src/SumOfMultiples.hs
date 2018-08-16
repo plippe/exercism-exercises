7fff58996ff997ce5430ac0075ea53a4337045e5
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-16 15:30:34 +0100

    mod faster than range + union

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,16 +1,7 @@
 module SumOfMultiples (sumOfMultiples) where
 
-import Data.List (nub, group, sort, union)
-import Data.Set (fromList, toList)
-
-concatDistinct :: Ord a => [[a]] -> [a]
--- concatDistinct = foldl union [] -- ~ 98 seconds
--- concatDistinct = nub . concat -- ~ 11 seconds
--- concatDistinct = toList . fromList . concat -- ~ 0.03 seconds
-concatDistinct = map head . group . sort . concat -- ~ 0.01 seconds
-
 multiples :: [Integer] -> Integer -> [Integer]
-multiples factors limit = concatDistinct $ map (\f -> [f, f*2 .. limit - 1]) factors
+multiples factors limit = filter (\n -> any ((==) 0 . mod n) factors) [1 .. limit - 1]
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples = (sum .) . multiples
