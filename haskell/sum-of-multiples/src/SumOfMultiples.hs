6c2c0d7e16f2463c9d764264483b2e5eb5ed7d1d
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-16 15:21:20 +0100

    Tested Set for distinct values

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,12 +1,13 @@
 module SumOfMultiples (sumOfMultiples) where
 
 import Data.List (nub, group, sort, union)
+import Data.Set (fromList, toList)
 
 concatDistinct :: Ord a => [[a]] -> [a]
--- concatDistinct = foldl union [] -- doesn't finish
+-- concatDistinct = foldl union [] -- ~ 98 seconds
 -- concatDistinct = nub . concat -- ~ 11 seconds
+-- concatDistinct = toList . fromList . concat -- ~ 0.03 seconds
 concatDistinct = map head . group . sort . concat -- ~ 0.01 seconds
-
 
 multiples :: [Integer] -> Integer -> [Integer]
 multiples factors limit = concatDistinct $ map (\f -> [f, f*2 .. limit - 1]) factors
