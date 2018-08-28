7c3e4f936939d53badb4ce37c2168454751c4534
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-28 21:27:07 +0100

    Improve solution with foldr

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,26 +1,12 @@
-module Acronym (abbreviate) where
+module Acronym where
 
 import Data.Char (isAlpha, isLower, isUpper, toUpper)
-import Data.List (unfoldr)
 
-isNotAlpha :: Char -> Bool
-isNotAlpha = not . isAlpha
-
-wordChar :: String -> Maybe (Char, String)
-wordChar [] = Nothing
-wordChar [x1] = Nothing
-wordChar (x1:x2:xs)
-    | isLower x1 && isUpper x2 = Nothing
-    | isNotAlpha x1 = Nothing
-    | isNotAlpha x2 = Just (x1, [])
-    | otherwise = Just (x1, x2:xs)
-
-words' :: String -> [String]
-words' [] = []
-words' xs = word:(words' rest)
-    where
-        word = unfoldr wordChar xs
-        rest = drop (length word + 1) xs
+letter :: (Char, Char) -> String -> String
+letter (x1, x2) xs
+    | (not . isAlpha) x1 && isAlpha x2 = x2:xs
+    | isLower x1 && isUpper x2         = x2:xs
+    | otherwise                        = xs
 
 abbreviate :: String -> String
-abbreviate =  map (toUpper . head) . filter (not . null) . words'
+abbreviate xs = map toUpper . foldr letter [] $ zip (' ':xs) xs
